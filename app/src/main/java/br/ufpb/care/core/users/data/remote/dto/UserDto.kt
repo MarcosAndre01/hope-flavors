453221db2e144984a80ca14655e2c71644c47e4c
Marcos André <marcosandre@cc.ci.ufpb.br>
2022-05-13 09:16:00 -0300

    Remover módulo não usado core

--- app/src/main/java/br/ufpb/care/core/users/data/remote/dto/UserDto.kt
+++
@@ -1,9 +0,0 @@
-package br.ufpb.care.core.users.data.remote.dto
-
-data class UserDto(
-    val id: String,
-    val userTypeID: String,
-    val email: String,
-    val firstName: String,
-    val lastName: String,
-)
