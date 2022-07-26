ad0a87f12a6e727e7082fda42b99bfbca41b17bd
Marcos André <marcosandre@cc.ci.ufpb.br>
2022-07-26 19:49:55 -0300

    Atualizar tipos do filhas

--- app/src/filhas/java/br/ufpb/care/register/UserKindsImpl.kt
+++ app/src/filhas/java/br/ufpb/care/register/UserKindsImpl.kt
@@ -3,9 +3,9 @@ package br.ufpb.care.register
 import br.ufpb.care.R
 
 class UserKindsImpl : UserKinds {
-    private val patient = UserKind(name = R.string.patient, description = R.string.patient_description, icon = R.drawable.logo)
-    private val caregiver = UserKind(name = R.string.caregiver, description = R.string.caregiver_description, icon = R.drawable.logo)
-    private val supportGroup = UserKind(name = R.string.support_group, description = R.string.support_group_description, icon = R.drawable.logo)
+    private val patient = UserKind(name = R.string.patient, description = R.string.patient_description, icon = R.drawable.patient)
+    private val caregiver = UserKind(name = R.string.caregiver, description = R.string.caregiver_description, icon = R.drawable.caregiver)
+    private val supportGroup = UserKind(name = R.string.support_group, description = R.string.support_group_description, icon = R.drawable.support_group)
 
-    override val value = listOf(patient, caregiver, supportGroup)
+    override val value = listOf(caregiver, supportGroup, patient)
 }
